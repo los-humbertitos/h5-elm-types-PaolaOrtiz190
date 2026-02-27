d379168fd12d8204f4cb22c9d018a711554ff4eb
PaolaOrtiz190 <up230168@alumnos.upa.edu.mx>
2026-02-27 15:00:47 -0600

    Tarea 5 se usa tipo personalizado y funciones para clasificar datos

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,52 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+gradeToStatus : Float -> GradeStatus
+gradeToStatus grade =
+    if grade < 0 then
+        Pending
+
+    else if grade > 7 then
+        Approved
+
+    else
+        Failed
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map gradeToStatus grades
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
